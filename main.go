2c1259535d766c837f4e94420dd8ae66df99537f
Luca <luca.boer@outlook.com>
2024-09-06 17:10:31 -0300

    removed aws tests

--- main.go
+++ main.go
@@ -5,48 +5,15 @@ import (
 	"log"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/dotenv-org/godotenvvault"
 	"github.com/robfig/cron/v3"
 )
-
-func CreateAWSSessionTest() (*session.Session, error) {
-	awsAccessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
-	if awsAccessKeyID == "" {
-		log.Println("AWS_ACCESS_KEY_ID is not set in environment variables")
-	} else {
-	}
-
-	awsSecretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
-	if awsSecretAccessKey == "" {
-		log.Println("AWS_SECRET_ACCESS_KEY is not set in environment variables")
-	} else {
-		log.Printf("AWS_SECRET_ACCESS_KEY retrieved: [SECURE]")
-	}
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1"),
-	})
-	if err != nil {
-		log.Printf("Fatal Error: Unable to create AWS session: %v", err)
-	}
-
-	fmt.Println(awsAccessKeyID)
-	fmt.Println(awsSecretAccessKey)
-
-	fmt.Println("Session created")
-
-	return sess, err
-}
 
 func main() {
 	err := godotenvvault.Load()
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
-
-	CreateAWSSessionTest()
 
 	secretName := fmt.Sprintf("%s/imaginereplay", os.Getenv("environment"))
 
